8ba1ce90cb7c5166d150ad348d020381d2cd6afb
Jack Nutkins <jnutkins@ndxc.tech>
2020-06-22 23:46:02 +0100

    speed test alpha

--- src/app/pages/speed-test/speed-test.component.ts
+++ src/app/pages/speed-test/speed-test.component.ts
@@ -3,6 +3,7 @@ import 'rxjs/add/observable/interval';
 import { Observable } from 'rxjs';
 import { package_builer_message } from '../../@core/data/package_builder_message';
 import { DataTransferService } from '../../@core/services/data-transfer.service';
+import { NbToastrService  } from '@nebular/theme';
 
 @Component({
   selector: 'speed-test',
@@ -11,12 +12,15 @@ import { DataTransferService } from '../../@core/services/data-transfer.service'
 })
 export class SpeedTestComponent implements OnInit {
 
-  constructor(private dataTransferService: DataTransferService) {
+  constructor(private dataTransferService: DataTransferService,
+    private toastrService: NbToastrService) {
 
   }
 
   sub: any;
   messages: any;
+
+  dataLastSent: string;
 
   losAngelesMessage: package_builer_message;
   newYorkMessage: package_builer_message;
@@ -26,10 +30,12 @@ export class SpeedTestComponent implements OnInit {
 
   ngOnInit() {
 
+    this.dataLastSent = 'N/A';
+
     // this.getResults();
 
-    this.sub = Observable.interval(3000)
-      .subscribe((val) => { this.getResults(); });
+    // this.sub = Observable.interval(3000)
+    //   .subscribe((val) => { this.getResults(); });
   }
 
   getResults() {
@@ -62,7 +68,16 @@ export class SpeedTestComponent implements OnInit {
         () => { });
   }
 
+  showToast(position, status, text) {
+    this.toastrService.show(
+      status || 'Success',
+      text,
+      { position, status });
+  }
+
   sendData() {
+
+    this.showToast('top-right', 'info', 'Sending Data...');
 
     this.dataTransferService.sendTestData()
       .subscribe(
@@ -71,8 +86,42 @@ export class SpeedTestComponent implements OnInit {
 
           console.log("Sent Data!");
 
+          this.showToast('top-right', 'success', 'Data Sent!');
+
+          this.dataLastSent = "21:44:02";
+
+          this.messages = data;
+          console.log(this.messages);
+
+          for (let entry of this.messages) {
+
+            this.dataLastSent = entry.datasent_time_string;
+
+            if (entry.contract_id === 1)
+              this.losAngelesMessage = entry;
+            else if (entry.contract_id === 2)
+              this.newYorkMessage = entry;
+            else if (entry.contract_id === 3)
+              this.londonMessage = entry;
+            else if (entry.contract_id === 4)
+              this.kyivMessage = entry;
+            else if (entry.contract_id === 5)
+              this.tokyoMessage = entry;
+
+          }
+
         },
-        err => console.error('Observer got an error: ' + err),
-        () => { });
+        err => {
+
+          console.error('Observer got an error: ' + err);
+
+          this.showToast('top-right', 'danger', 'Failed to Send Data!');
+
+          this.dataLastSent = 'N/A';
+
+        },
+        () => { 
+
+        });
   }
 }
